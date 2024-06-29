b56fda5180a091a20a6637571e381939cf95c60a
Kiel <kielbermudez200104@gmail.com>
2024-06-30 02:46:06 +0800

    test video

--- resources/js/Pages/Sidebar.vue
+++ resources/js/Pages/Sidebar.vue
@@ -7,7 +7,7 @@
             <ul class="list-group">
                 <Link href="/dashboard"><li class="list-group-item text-center">Home</li></Link>
                 <Link href="/create_product_view"><li class="list-group-item text-center">Create</li></Link>
-                <Link href="/video/video1.mp4"><li class="list-group-item text-center">Video</li></Link>
+                <Link href="/video/video1.mp4"><li class="list-group-item text-center">Videos Link</li></Link>
 
               </ul>
         </div>
--- resources/js/Pages/Video.vue
+++ resources/js/Pages/Video.vue
@@ -20,15 +20,6 @@
                     :src="`/videos/${props.res.original}`"
                     type="video/mp4"
                 />
-                <p class="vjs-no-js">
-                    To view this video please enable JavaScript, and consider
-                    upgrading to a web browser that
-                    <a
-                        href="https://videojs.com/html5-video-support/"
-                        target="_blank"
-                        >supports HTML5 video</a
-                    >
-                </p>
             </video>
         </div>
     </div>
